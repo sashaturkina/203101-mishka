85d6ba47c1764548b328a68ef718cacc6b467cf2
sasha <sashaturkina.93@mail.ru>
2017-06-23 16:12:31 +0300

    .

--- js/scriptmenu.js
+++ js/scriptmenu.js
@@ -1,8 +1,5 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
-var link = document.querySelector(".featured__btn");
-var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal--close");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
@@ -12,14 +9,4 @@ navToggle.addEventListener("click", function() {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
   }
-});
-
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("modal--opened");
-});
-
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.remove("modal--open");
 });
